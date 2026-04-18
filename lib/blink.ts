65e36370bad526132802839fce4ef77d12809c82
claudiacorallini86-debug <claudiacorallini86@gmail.com>
2026-04-18 19:23:21 +0200

    [Blink] Forzatura sincronizzazione GitHub • Aggiunto commento di versione in lib/blink.ts per triggerare il push • Conferma migrazione a Blink Native Auth per supporto Expo Go e Web

--- lib/blink.ts
+++ lib/blink.ts
@@ -2,6 +2,7 @@ import { createClient, AsyncStorageAdapter } from '@blinkdotnew/sdk'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as WebBrowser from 'expo-web-browser'
 
+// v2.1: Native Auth Migration for Expo Go
 export const blink = createClient({
   projectId: process.env.EXPO_PUBLIC_BLINK_PROJECT_ID!,
   publishableKey: process.env.EXPO_PUBLIC_BLINK_PUBLISHABLE_KEY!,
